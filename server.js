fe335ea224991d468b944853f9aebc2576da3e4f
RMAbdullah6212 <abdullahaslam6212@gmail.com>
2026-07-01 15:05:10 +0500

    commit

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// Don't call app.listen() on Vercel
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 4000}`);
+  });
+}
+
+export default app;
